49ddf4934ff8a19e47cf74402f925db22887314d
Sarah Ji <sarahh.jii@gmail.com>
2018-01-26 23:59:48 +0000

    commit and push the autosim and runsim scripts into the correct hw 1 file on github

---
+++ hw1/autoSim.R
@@ -0,0 +1,15 @@
+# autoSim.R
+
+nVals = seq(100, 500, by=100)
+distTypes = c("gaussian", "t1", "t5")
+
+for (n in nVals) {
+  for (dist in distTypes){
+    oFile = paste("n", n, dist, ".txt", sep="")
+    dist_arg = paste("dist=", "'", dist, "'", sep = "")
+    arg = paste("n=", n, sep="", " \"", dist_arg, "\"", " seed=", 280, " rep=", 50)
+    sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile)
+    system(sysCall)
+    print(paste("sysCall=", sysCall, sep=""))
+  }
+}
